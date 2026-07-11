58f9e60e02f6fbe3816239080ac4f0f9a8a92e45
Sindre Sorhus <sindresorhus@gmail.com>
2026-07-11 20:50:52 +0200

    : Cover all fields with

--- test/prefer-exports.js
+++ test/prefer-exports.js
@@ -15,8 +15,8 @@ test.snapshot({
 		'{"browser": "./index.browser.js"}',
 		'{"types": "./index.d.ts"}',
 		'{"typings": "./index.d.ts"}',
-		// These fields are still reported when `exports` is present.
-		'{"exports": "./index.js", "main": "./index.js", "types": "./index.d.ts", "browser": {"./server.js": "./browser.js"}}',
+		// Every field is still reported when `exports` is present.
+		'{"exports": "./index.js", "main": "./index.js", "module": "./index.mjs", "browser": {"./server.js": "./browser.js"}, "types": "./index.d.ts", "typings": "./index.d.ts"}',
 		// Multiple legacy fields.
 		'{"main": "./index.js", "module": "./index.mjs", "types": "./index.d.ts"}',
 	],
